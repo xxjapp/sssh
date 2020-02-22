6d4e0db28de75a749d19225250211ce70b549df7
nmaeda <nmaeda@keepdata.asia>
2020-02-22 12:13:48 +0900

    rsync log files to remte host

--- sssh.rb
+++ sssh.rb
@@ -16,25 +16,29 @@ require_relative "os"
 
 class Sssh
     def initialize params
-        @user           = params["user"]
-        @host           = params["host"]
-        @port           = params["port"]
-        @remote_port    = params["remote_port"]
-        @local_port     = params["local_port"]
+        @user               = params["user"]
+        @host               = params["host"]
+        @port               = params["port"]
+        @remote_port        = params["remote_port"]
+        @local_port         = params["local_port"]
+        @ssh_path           = params["ssh_path"]
+        @remote_log_path    = params["remote_log_path"]
 
-        @cmd    = "ssh -NT -R #{@remote_port}:localhost:#{@local_port} #{use_at_host} -p #{@port}"      # cmd to create reverse ssh tunneling
-        @cmd2   = "ssh #{use_at_host} -p #{@port} \"netstat -ano | grep :#{@remote_port} | wc -l\""     # cmd to check the created reverse ssh tunneling
+        @cmd    = "ssh -NT -R #{@remote_port}:localhost:#{@local_port} #{user_at_host} -p #{@port}"         # cmd to create reverse ssh tunneling
+        @cmd2   = "ssh #{user_at_host} -p #{@port} \"netstat -ano | grep :#{@remote_port} | wc -l\""        # cmd to check the created reverse ssh tunneling
+        @cmd3   = "rsync -avzhe '#{@ssh_path} -p #{@port}' #{@log_dir} #{user_at_host}:#{@remote_log_path}" # cmd to rsync log to remote host
 
         @log_dir = params["log_dir"]
         $log ||= LoggingX.get_log File.basename(__FILE__), log_dir: @log_dir, log_level: :debug, pattern: "%d %-5l [%X{tid}] (%F:%L) %M - %m\n"
     end
 
-    def use_at_host
+    def user_at_host
         @user ? "#{@user}@#{@host}" : @host
     end
 
     def start
         start_monitor
+        start_rsync_log_file
 
         loop {
             OS.popen3(@cmd, listener: self) { |o, e|
@@ -87,9 +91,6 @@ class Sssh
 
                 process_info = @process_info
                 kill_current_connection process_info if !status_ok? process_info
-
-                # NOTE: touch log_dir to force cloud storage service like Dropbox to upload log files
-                FileUtils.touch Dir.glob "#{@log_dir}/*.log"
             }
         }
     end
@@ -131,6 +132,24 @@ class Sssh
 
         res.to_i >= 2
     end
+
+    def start_rsync_log_file
+        Thread.new {
+            Logging.mdc['tid'] = LoggingX.get_current_thread_id "rsync_log"
+
+            loop {
+                sleep 60
+
+                # NOTE: touch log_dir to force cloud storage service like Dropbox to upload log files
+                FileUtils.touch Dir.glob "#{@log_dir}/*.log"
+
+                OS.popen3(@cmd3) { |o, e|
+                    $log.info  o.chomp if o
+                    $log.error e.chomp if e
+                }
+            }
+        }
+    end
 end
 
 ################################################################
@@ -138,7 +157,7 @@ end
 
 if __FILE__ == $0
     require 'optparse'
-    params = ARGV.getopts nil, "user:", "host:localhost", "port:22", "remote_port:20022", "local_port:22", "log_dir:/var/log/ruby"
+    params = ARGV.getopts nil, "user:", "host:localhost", "port:22", "remote_port:20022", "local_port:22", "ssh_path:C:/cygwin64/bin/ssh.exe", "remote_log_path:/tmp/log/", "log_dir:/var/log/ruby"
 
     Logging.mdc['tid'] = LoggingX.get_current_thread_id "main"
     Sssh.new(params).start
